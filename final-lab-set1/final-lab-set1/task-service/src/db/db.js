4c503d3e228a66780a83f725e11fc52b1e367933
Atiroj049 <Atirojframe@gmail.com>
2026-03-16 09:53:03 +0700

    Delete duplicate files.

--- final-lab-set1/final-lab-set1/task-service/src/db/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  host:     process.env.DB_HOST || 'postgres',
-  port:     parseInt(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME || 'taskboard',
-  user:     process.env.DB_USER || 'admin',
-  password: process.env.DB_PASSWORD || 'secret123',
-});
-module.exports = { pool };
